1196038b34aca72bcd1a5ba0e0a064e84d469b65
zigzagdev <msts.oo0131@gmail.com>
2022-10-30 14:36:42 +0900

    LoginCode_fix

--- app/Http/Requests/Api/LoginRequest.php
+++ app/Http/Requests/Api/LoginRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests\Api;
 
+use App\Consts\Api\MessageConst;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class LoginRequest extends FormRequest
 {
@@ -13,7 +16,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -44,5 +47,21 @@ class LoginRequest extends FormRequest
             'string' => ':attributeの値が不正です。',
             'email' => 'メールアドレスの形式が正しくありません。',
         ];
+    }
+
+
+    // Not to send API Response 302, write down these codes.
+    protected function failedValidation(validator $validator)
+    {
+        $errors = $validator->errors()->toArray();
+
+        // resetFunction is get the first element form $errors Array.
+        $response = [
+            'statusCode'  => MessageConst::Bad_Request,
+            'statusMessage' => reset($errors)[0]
+        ];
+        throw new HttpResponseException(
+            response()->json($response, MessageConst::Bad_Request)
+        );
     }
 }
