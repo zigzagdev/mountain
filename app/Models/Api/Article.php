c43281c383582c6f0bf3d0ac84fb9466d2b61bb7
zigzagdev <msts.oo0131@gmail.com>
2022-11-12 12:09:57 +0900

    queryBuilder_fix

--- app/Models/Api/Article.php
+++ app/Models/Api/Article.php
@@ -1,11 +1,20 @@
 <?php
-namespace App\Models;
+namespace App\Models\Api;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
-    protected $guarded = ['id', 'created_at', 'updated_at', 'admin_id'];
+    protected $guarded = ['id', 'adminId'];
+    protected $dates = ['deleted_at'];
+    use SoftDeletes;
 
+
+    public function selectedArticle()
+    {
+        $selectArticle = Article::get('deleted_at', '<', Carbon::now());
+    }
 }
