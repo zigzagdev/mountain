186befd709f72d8de6bb9d5b364a7a5960add3c7
zigzagdev <msts.oo0131@gmail.com>
2022-11-19 10:23:49 +0900

    articleCreate

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Consts\Api\MessageConst;
+use App\Http\Resources\Api\RegisterArticleResource;
+use App\Models\Api\Admin;
 use App\Consts\CommonConst;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\ErrorResource;
 use App\Http\Requests\Api\ArticleRequest;
-use App\Http\Resources\Api\RegisterArticleResource;
+use App\Http\Resources\Api\RegisterArticleCollection;
 use App\Models\Api\Article;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,12 +20,12 @@ use Carbon\Carbon;
 class ArticleController extends Controller
 {
     // 記事投稿
-    public function articleWriting(ArticleRequest $request){
+    public function articleWrite(ArticleRequest $request){
         try {
             $adminId = $request->adminId;
 
             // Whether admin is existed or not .
-            $admin = Article::where('id', $adminId)->first();
+            $admin = Admin::where('id', $adminId)->first();
             if (!$admin) {
                 $request->merge(['statusMessage' => CommonConst::ERR_05]);
                 return new ErrorResource($request, Response::HTTP_BAD_REQUEST);
@@ -35,16 +37,20 @@ class ArticleController extends Controller
                 'statusMessage' => MessageConst::OK,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-                'userToken' => $request->userToken
+                'adminId' => $adminId,
             ]);
-
             return new RegisterArticleResource($request);
         } catch (\Exception $e) {
             DB::rollBack();
             $request->merge(['statusMessage' => "記事の投稿に失敗致しました。"]);
-            return new ErrorResource($request, MessageConst::Not_Acceptable);
+            $statusMessage = $e->getMessage();
+            var_dump($statusMessage);
+            return new ErrorResource($request, Response::HTTP_BAD_REQUEST);
         }
     }
+
+
+    // softDelete is needed ??
 
     //記事編集
 //    public function articleReWrite($adminId, ArticleRequest $request)
