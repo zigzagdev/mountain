f77298a2a28f4ceeb19a53e0268051d5e93ac8dc
zigzagdev <msts.oo0131@gmail.com>
2022-11-23 11:10:52 +0900

    middleTable_create

---
+++ app/Models/Api/MountainRating.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Api;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MountainRating extends Model
+{
+    protected $table = 'total_rating';
+    protected $guarded = [];
+
+
+}
---
+++ database/migrations/2022_11_23_000327_create_total_rating_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTotalRatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('total_rating', function (Blueprint $table) {
+            $table->string('mountainRate', 100);
+            $table->integer('mountainName');
+            $table->unsignedBigInteger('admin_id')->comment('adminId');
+            $table->timestamps();
+            $table->foreign('admin_id')->references('adminId')->on('articles')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('total_rating');
+    }
+}
